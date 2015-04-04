750468002c8ec589702b246ddaf478a865c1fd04
Brad Montgomery <brad@bradmontgomery.net>
2015-04-04 11:58:03 -0500

    updating tests

--- ninethreesix/tests.py
+++ ninethreesix/tests.py
@@ -6,10 +6,10 @@ def test_init():
     p = Password()
 
     # Test value of default parameters
-    eq_(p.num_words, 4)
+    eq_(p.num_words, 3)
     eq_(p.min_len, 3)
-    eq_(p.max_len, 8)
-    eq_(len(p.content), 2493082)  # should be a string of this length
+    eq_(p.max_len, 6)
+    eq_(len(p.content), 805232)  # should be a string of this length
     eq_(p.content.find("\n"), -1)  # should contain no newlines
 
 
@@ -17,11 +17,11 @@ def test_password():
     p = Password()
     result = p.password()
     eq_(type(result), list)
-    eq_(len(result), 4)
+    eq_(len(result), 3)
 
 
 def test_as_string():
     p = Password()
     result = p.as_string()
     eq_(type(result), str)  # should return a sring
-    eq_(result.count("-"), 3)  # count the delimiters
+    eq_(result.count("-"), 2)  # count the delimiters
